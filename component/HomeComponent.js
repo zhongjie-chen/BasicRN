2e6e171839074f605a6fd64b9e2ccbfaf0ac4f4e
zhongjie-chen <czjchn@163.com>
2016-03-10 16:54:58 +0800

    轮播图1.0.0

--- component/HomeComponent.js
+++ component/HomeComponent.js
@@ -3,8 +3,10 @@ import React,{
     Text,
     WebView,
     Picker,
+    Image,
     StyleSheet,
     AsyncStorage,
+    ViewPagerAndroid,
     TouchableOpacity
 } from 'react-native'
 
@@ -15,6 +17,10 @@ class HomeComponent extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state={
+      pageCount:3,
+      index:0
+    }
   }
 
   _handlerLogin(e){
@@ -43,18 +49,77 @@ class HomeComponent extends React.Component {
         })
     }
   }
+
   render() {
+      let views = [];
+      let img;
+      for (let i = 0; i < this.state.pageCount; i++) {
+        switch (i%3) {
+          case 0:
+            img = <Image style={{}} source={require('../images/bg_header_1.png')} />
+            break;
+          case 1:
+            img = <Image style={{}} source={require('../images/bg_header_2.png')} />
+            break;
+          case 2:
+            img = <Image style={{}} source={require('../images/bg_header_3.png')} />
+            break;
+          default:
+
+        }
+        views.push(
+          <View>
+            {img}
+          </View>
+        );
+      }
+      let docts;
+      switch (this.state.index % 3) {
+        case 0:
+      docts=[  <Image style={{margin:4}} source={require('../images/bg_dot_focused.png')} />,
+        <Image style={{margin:4}} source={require('../images/bg_dot_normal.png')} />,
+        <Image style={{margin:4}} source={require('../images/bg_dot_normal.png')} />];
+          break;
+        case 1:
+      docts=[    <Image style={{margin:4}} source={require('../images/bg_dot_normal.png')} />,
+        <Image style={{margin:4}} source={require('../images/bg_dot_focused.png')} />,
+        <Image style={{margin:4}} source={require('../images/bg_dot_normal.png')} />];
+          break;
+        case 2:
+      docts=[   <Image style={{margin:4}} source={require('../images/bg_dot_normal.png')} />,
+        <Image style={{margin:4}} source={require('../images/bg_dot_normal.png')} />,
+        <Image style={{margin:4}} source={require('../images/bg_dot_focused.png')} />];
+          break;
+        default:
+
+      }
+      //setInterval(()=>this.viewPager.setPage(this.state.index+1),2000);
+
     return(
-        <View style={styles.container}>
-          <TouchableOpacity style={styles.leftView} onPress={()=>this._handlerArticle()} >
-            <Text>左边</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.middleView}>
-            <Text>中间</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.rightView} onPress={()=>this._handlerLogin()}>
-            <Text>登录</Text>
-          </TouchableOpacity>
+        <View style={{flex:1}}>
+          <View style={styles.container}>
+            <TouchableOpacity style={styles.leftView} onPress={()=>this._handlerArticle()} >
+              <Text>左边</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.middleView}>
+              <Text>中间</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.rightView} onPress={()=>this._handlerLogin()}>
+              <Text>登录</Text>
+            </TouchableOpacity>
+          </View>
+
+          <ViewPagerAndroid style={{height:205}} initialPage={0}
+            ref={viewPager => { this.viewPager = viewPager; }}
+            onPageSelected ={(event)=>this.setState({index:event.nativeEvent.position})}
+          >
+              {views}
+          </ViewPagerAndroid>
+          <View style={{backgroundColor:"rgba(34, 26, 38, 0.5)",height:30,marginTop:-30,flexDirection:'row',alignItems:'center',justifyContent:'flex-end'}}>
+            <Text style={{flex:1,color:'white'}}>welcome to RN thnks!</Text>
+            {docts}
+          </View>
+
         </View>
     );
   }
